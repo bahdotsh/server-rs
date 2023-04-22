b02923e295cb2be8addaed1e8ebea4fc0374f4c1
bahdotsh <appu.yess@gmail.com>
2023-04-22 19:03:19 +0530

    handle requests properly

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
@@ -14,14 +14,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let bufreader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = bufreader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    
-    let status_line = "Http/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let request_line = bufreader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
